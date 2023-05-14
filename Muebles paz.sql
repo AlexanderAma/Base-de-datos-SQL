728f2f8bd24b0516b70914cbfc03517b95c3633f
AlexanderAma <121409196+AlexanderAma@users.noreply.github.com>
2023-05-14 09:32:01 -0300

    Add files via upload

---
+++ Muebles paz.sql
@@ -0,0 +1,111 @@
+CREATE SCHEMA muebles_paz;
+USE muebles_paz;
+
+CREATE TABLE Pais (
+	Pais_id INT NOT NULL AUTO_INCREMENT,
+    Nombre VARCHAR(45) NOT NULL,
+    PRIMARY KEY (Pais_id)
+);
+
+CREATE TABLE Provincia (
+	Provincia_id INT NOT NULL AUTO_INCREMENT,
+    Nombre VARCHAR(45) NOT NULL,
+    PRIMARY KEY (Provincia_id)
+);
+
+CREATE TABLE Ciudad (
+	Ciudad_id INT NOT NULL AUTO_INCREMENT,
+    Nombre VARCHAR(45) NOT NULL,
+    PRIMARY KEY (Ciudad_id)
+);
+
+CREATE TABLE Transportista (
+	Transportista_id INT NOT NULL AUTO_INCREMENT,
+    Nombre VARCHAR(45),
+    Apellido VARCHAR(45),
+    Documento INT NOT NULL,
+    telefono INT NOT NULL,
+    Ciudad_id INT NOT NULL,
+    Provincia_id INT NOT NULL,
+    PRIMARY KEY (Transportista_id),
+    FOREIGN KEY (Ciudad_id) REFERENCES Ciudad (Ciudad_id),
+    FOREIGN KEY (Provincia_id) REFERENCES Provincia (Provincia_id)
+);
+
+CREATE TABLE cliente(
+	Cliente_id INT NOT NULL AUTO_INCREMENT, 
+    Nombre VARCHAR(45) NOT NULL,
+    Apellido VARCHAR (45) NOT NULL,
+    Direccion VARCHAR (45) NOT NULL,
+    Ciudad_id INT NOT NULL,
+    Provincia_id INT NOT NULL,
+    Pais_id INT NOT NULL,
+    Telefono INT,
+    PRIMARY KEY (Cliente_id),
+    FOREIGN KEY (Ciudad_id) REFERENCES Ciudad (Ciudad_id),
+    FOREIGN KEY (Provincia_id) REFERENCES Provincia (Provincia_id),
+    FOREIGN KEY (Pais_id) REFERENCES Pais (Pais_id)
+);
+
+CREATE TABLE Proveedores(
+	Proveedores_id INT NOT NULL AUTO_INCREMENT, 
+    Razon_Social VARCHAR(45) NOT NULL,
+    Direccion VARCHAR(45) NOT NULL,
+    Ciudad_id INT NOT NULL,
+    Provincia_id INT NOT NULL,
+    Pais_id INT NOT NULL,
+    Telefono INT NOT NULL,
+    Email varchar(45) NOT NULL,
+    PRIMARY KEY (proveedores_id),
+    FOREIGN KEY (Ciudad_id) REFERENCES Ciudad (Ciudad_id),
+    FOREIGN KEY (Provincia_id) REFERENCES Provincia (Provincia_id)
+);
+
+CREATE TABLE Categoria(
+	Categoria_id INT NOT NULL AUTO_INCREMENT, 
+    Nombre_producto VARCHAR(45) NOT NULL,
+    PRIMARY KEY (categoria_id)
+    
+);
+
+CREATE TABLE Producto (
+	Producto_id INT NOT NULL AUTO_INCREMENT, 
+	Nombre_producto VARCHAR(45) NOT NULL,
+	Proveedores_id INT NOT NULL,
+	Precio_unitario INT NOT NULL,
+	Stock INT NOT NULL,
+	Categoria_id INT NOT NULL,
+	PRIMARY KEY (producto_id),
+	FOREIGN KEY (categoria_id) REFERENCES Categoria(categoria_id),
+    FOREIGN KEY (proveedores_id) REFERENCES Proveedores(proveedores_id)
+);
+
+
+CREATE TABLE Orden (
+	Orden_id INT NOT NULL AUTO_INCREMENT, 
+    Nombre_producto VARCHAR(45) NOT NULL,
+    Fecha_orden date NOT NULL,
+    Cliente_id int NOT NULL,
+    PRIMARY KEY (orden_id),
+	FOREIGN KEY (cliente_id) REFERENCES Cliente (cliente_id)
+);
+
+CREATE TABLE Detalle_orden (
+	Detalle_id INT NOT NULL AUTO_INCREMENT, 
+    Producto_id INT,
+    Orden_id INT,
+    Transportista_id INT NOT NULL,
+    Precio_unitario INT NOT NULL,
+    Cantidad INT NOT NULL,
+    Descuento INT NOT NULL,
+    PRIMARY KEY (detalle_id), 
+    FOREIGN KEY (producto_id) REFERENCES Producto (producto_id),
+    FOREIGN KEY (orden_id) REFERENCES Orden(orden_id),
+    FOREIGN KEY (Transportista_id) REFERENCES Transportista(Transportista_id)
+);
+
+
+
+
+
+
